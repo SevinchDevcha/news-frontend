d14bd8582084d66db321de240020f41529eadf50
Sevinch <erkinovasevinch38@gmail.com>
2025-09-16 10:23:09 +0500

    loading

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    outDir: 'build',   // <— dist o‘rniga build papkasi bo‘ladi
-  },
+  }
 })
