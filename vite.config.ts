58167d1d06c232ea1f0826483a5c504b452b339a
Sevinch <erkinovasevinch38@gmail.com>
2025-09-16 09:22:35 +0500

    loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-// https://vite.dev/config/
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'build',   // <— dist o‘rniga build papkasi bo‘ladi
   },
 })
